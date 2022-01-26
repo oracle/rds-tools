oracle/rds-tools

commit a437a5b1d750f4cdc4ce316a408714ee38bc2561

rds-info: Display destination qp number
This patch provides the user-space modifications required to display the
destination qp number corresponding to an rds connection. Essential changes
are made in the kernel to extract the destination qp number of a connection
and pass it to user-space. Invalid qp number is being provided the value of
-1 instead of 0 which was being used earlier, as 0 corresponds to a special
qp.

Orabug: 31940566

Suggested-by: Rama Nichanamatlu <rama.nichanamatlu@oracle.com>
Signed-off-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>
Reviewed-by: Alan Maguire <alan.maguire@oracle.com>

rds-info.c
@@ -319,8 +319,11 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 	       prt_width, "LocalAddr", prt_width, "RemoteAddr", "Tos", "SL",
 	       "LocalDev", "RemoteDev");
 
-	if (each >= info_len) {
-		printf("  QPNo");
+	if (each < info_len) {
+		if (info_len - each ==  sizeof(ic.dst_qp_num))
+			printf("%10s", "QPNo");
+	} else {
+		printf("%10s%10s", "SrcQPNo", "DstQPNo");
 	}
 
 	printf("\n");
@@ -334,9 +337,13 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 			       ipv6addr(ic6.src_gid),
 			       ipv6addr(ic6.dst_gid));
 
-			if (each >= info_len) {
-				printf("  %d", ic6.qp_num);
+			if (each < info_len) {
+				if (info_len - each ==  sizeof(ic6.dst_qp_num))
+					printf("%10d", ic6.qp_num);
+			} else {
+				printf("%10d%10d", ic6.qp_num, ic6.dst_qp_num);
 			}
+
 			if (opt_verbose) {
 				printf("  send_wr=%u", ic6.max_send_wr);
 				printf(", recv_wr=%u", ic6.max_recv_wr);
@@ -357,9 +364,13 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 			       ipv6addr(ic.src_gid),
 			       ipv6addr(ic.dst_gid));
 
-			if (each >= info_len) {
-				printf("  %d", ic.qp_num);
+			if (each < info_len) {
+				if (info_len - each ==  sizeof(ic.dst_qp_num))
+					printf("%10d", ic.qp_num);
+			} else {
+				printf("%10d%10d", ic.qp_num, ic.dst_qp_num);
 			}
+
 			if (opt_verbose) {
 				printf("  send_wr=%u", ic.max_send_wr);
 				printf(", recv_wr=%u", ic.max_recv_wr);

include/rds.h
@@ -296,9 +296,10 @@ struct rds_info_rdma_connection {
 	uint32_t	frag;
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
-	uint32_t        qp_num;
+	int32_t		qp_num;
 	uint32_t        w_alloc_ctr;
 	uint32_t        w_free_ctr;
+	int32_t		dst_qp_num;
 
 };
 
@@ -319,9 +320,10 @@ struct rds6_info_rdma_connection {
 	uint32_t	frag;
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
-	uint32_t	qp_num;
+	int32_t		qp_num;
 	uint32_t	w_alloc_ctr;
 	uint32_t	w_free_ctr;
+	int32_t		dst_qp_num;
 };
 
 /*

commit 8119dc19f2c9f3976dc3acd7b653c57589f5f748

rds-info: Fixed missing documentation for rds-info -I
A previous patch added the dst_qp_num as a displayable field to the
functionality of rds-info -I, but did not add the corresponding
documentation to the man page.

This patch modifies the code slightly to display a zero value for the
field instead of not printing the field when running on an older kernel
that does not support the field. It also documents these fields in the
man page.

Orabug: 33771194

Fixes: a437a5b1d750 ("rds-info: Display destination qp number")
Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
Reviewed-by: Manjunath Patil <manjunath.b.patil@oracle.com>

rds-info.1
@@ -134,6 +134,10 @@ The remote IP address of this connection.
 The local IB Global Identifier, printed in IPv6 address syntax.
 .It RemoteDev
 The remote IB Global Identifier, printed in IPv6 address syntax.
+.It SrcQPNo
+The source QP number for this connection.
+.It DstQPNo
+The destination QP number for this connection.
 .El
 
 If verbose output is requested, per-connection settings such as the

rds-info.c
@@ -426,8 +426,6 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 	struct rds6_info_rdma_connection ic6;
 	struct rds_info_rdma_connection ic;
 	int prt_width;
-	int info_len;
-	info_len = sizeof(struct rds_info_rdma_connection);
 
 	if (prt_ipv6)
 		prt_width = PRT_IPV6_WIDTH;
@@ -435,34 +433,22 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 		prt_width = PRT_IPV4_WIDTH;
 
 
-	printf("\nRDS IB Connections:\n%*s %*s %4s %3s %32s %32s",
+	printf("\nRDS IB Connections:\n%*s %*s %4s %3s %32s %32s %10s %10s",
 	       prt_width, "LocalAddr", prt_width, "RemoteAddr", "Tos", "SL",
-	       "LocalDev", "RemoteDev");
-
-	if (each < info_len) {
-		if (info_len - each ==  sizeof(ic.dst_qp_num))
-			printf("%10s", "QPNo");
-	} else {
-		printf("%10s%10s", "SrcQPNo", "DstQPNo");
-	}
+	       "LocalDev", "RemoteDev", "SrcQPNo", "DstQPNo");
 
 	printf("\n");
 
 	if (prt_ipv6) {
 		for_each(ic6, data, each, len) {
-			printf("%*s %*s %4u %3u %32s %32s",
+			printf("%*s %*s %4u %3u %32s %32s %10d %10d",
 			       prt_width, ipaddr(&ic6.src_addr, prt_ipv6),
 			       prt_width, ipaddr(&ic6.dst_addr, prt_ipv6),
 			       ic6.tos, ic6.sl,
 			       ipv6addr(ic6.src_gid),
-			       ipv6addr(ic6.dst_gid));
-
-			if (each < info_len) {
-				if (info_len - each ==  sizeof(ic6.dst_qp_num))
-					printf("%10d", ic6.qp_num);
-			} else {
-				printf("%10d%10d", ic6.qp_num, ic6.dst_qp_num);
-			}
+			       ipv6addr(ic6.dst_gid),
+			       ic6.qp_num,
+			       ic6.dst_qp_num);
 
 			if (opt_verbose) {
 				printf("  send_wr=%u", ic6.max_send_wr);
@@ -477,19 +463,14 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 		}
 	} else {
 		for_each(ic, data, each, len) {
-			printf("%*s %*s %4u %3u %32s %32s",
+			printf("%*s %*s %4u %3u %32s %32s %10d %10d",
 			       prt_width, ipaddr(&ic.src_addr, prt_ipv6),
 			       prt_width, ipaddr(&ic.dst_addr, prt_ipv6),
 			       ic.tos, ic.sl,
 			       ipv6addr(ic.src_gid),
-			       ipv6addr(ic.dst_gid));
-
-			if (each < info_len) {
-				if (info_len - each ==  sizeof(ic.dst_qp_num))
-					printf("%10d", ic.qp_num);
-			} else {
-				printf("%10d%10d", ic.qp_num, ic.dst_qp_num);
-			}
+			       ipv6addr(ic.dst_gid),
+			       ic.qp_num,
+			       ic.dst_qp_num);
 
 			if (opt_verbose) {
 				printf("  send_wr=%u", ic.max_send_wr);
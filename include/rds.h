oracle/rds-tools

commit eb793ecd0fef8c989b1c9623f02e7d35626804a4

rds-tools: Added '-p' option to rds-info to show paths
Two new socket options RDS6_INFO_CONN_PATHS, RDS_INFO_CONN_PATHS have been
added to the kernel to provide information about the underlying paths of an
RDS connection. rds-info now has a new '-p' option that uses these new
options to display information on the paths.

Orabug: 31650130

Signed-off-by: Rao Shoaib <rao.shoaib@oracle.com>
Reviewed-by: Mark Haywood <mark.haywood@oracle.com>

include/rds.h
@@ -41,6 +41,8 @@
 
 #include <linux/types.h>
 #include <limits.h>
+#include <linux/net.h>
+#include <time.h>
 /* XXX <net/sock.h> was included as part of NETFILTER support (commit f13bbf62)
  * but <net/sock.h> is not exported to uapi, although <linux/rds.h> is
  * (in theory). Is <net/sock.h> needed for user-apps that use netfilter?
@@ -174,8 +176,10 @@ struct rds_cmsg_rx_trace {
 #define RDS6_INFO_SOCKETS		10015
 #define RDS6_INFO_TCP_SOCKETS		10016
 #define RDS6_INFO_IB_CONNECTIONS	10017
+#define RDS_INFO_CONN_PATHS		10020
+#define RDS6_INFO_CONN_PATHS		10021
 
-#define RDS_INFO_LAST			10017
+#define RDS_INFO_LAST			10021
 
 struct rds_info_counter {
 	u_int8_t	name[32];
@@ -324,6 +328,71 @@ struct rds6_info_rdma_connection {
 	uint32_t	w_alloc_ctr;
 	uint32_t	w_free_ctr;
 	int32_t		dst_qp_num;
+};
+
+struct rds_path_info {
+        time_t        attempt_time;
+        time_t        connect_time;
+        time_t        reset_time;
+        uint32_t        disconnect_reason;
+        uint32_t        connect_attempts;
+        unsigned int    index;
+        u_int8_t        flags;
+} __attribute__((packed));
+
+struct rds_info_connection_paths {
+        struct in6_addr local_addr;
+        struct in6_addr peer_addr;
+        u_int8_t        transport[TRANSNAMSIZ];
+        uint8_t         tos;
+        u_int8_t        npaths;
+        struct rds_path_info paths[];
+} __attribute__((packed));
+
+#define MAC_DISCON_REASON       (sizeof(conn_drop_reasons)/sizeof(char *))
+char *conn_drop_reasons[] = {
+	"--",
+	"user reset",
+	"invalid connection state",
+	"failure to move to DOWN state",
+	"connection destroy",
+	"conn_connect failure",
+	"hb timeout",
+	"reconnect timeout",
+	"cancel operation on socket",
+	"race between ESTABLISHED event and drop",
+	"conn is not in CONNECTING state",
+	"qp event",
+	"incoming REQ in CONN_UP state",
+	"incoming REQ in CONNECTING state",
+	"passive setup_qp failure",
+	"rdma_accept failure",
+	"active setup_qp failure",
+	"rdma_connect failure",
+	"resolve_route failure",
+	"detected rdma_cm_id mismatch",
+	"ROUTE_ERROR event",
+	"ADDR_ERROR event",
+	"CONNECT_ERROR or UNREACHABLE or DEVICE_REMOVE event",
+	"CONSUMER_DEFINED reject",
+	"REJECTED event",
+	"ADDR_CHANGE event",
+	"DISCONNECTED event",
+	"TIMEWAIT_EXIT event",
+	"post_recv failure",
+	"send_ack failure",
+	"no header in incoming msg",
+	"corrupted header in incoming msg",
+	"fragment header mismatch",
+	"recv completion error",
+	"send completion error",
+	"post_send failure",
+	"rds_rdma module unload",
+	"active bonding failover",
+	"corresponding loopback conn drop",
+	"active bonding failback",
+	"sk_state to TCP_CLOSE",
+	"tcp_send failure",
 };
 
 /*

rds-info.c
@@ -53,9 +53,10 @@
 
 /* WHUPS changed the struct rds_info_connection definition b/w rds in 1.4 & 1.5. gotta support both
    for now. TODO remove check of transport[15] once ofed pre-1.5 is extinct. */
-#define rds_conn_flag(conn, flag, letter) \
-	(conn.flags & RDS_INFO_CONNECTION_FLAG_##flag \
-	|| conn.transport[15] & RDS_INFO_CONNECTION_FLAG_##flag ? letter : '-')
+
+#define rds_conn_flag(conn_flags, flag, transport, letter) \
+	(conn_flags & RDS_INFO_CONNECTION_FLAG_##flag \
+	|| transport[15] & RDS_INFO_CONNECTION_FLAG_##flag ? letter : '-')
 
 #define min(a, b) (a < b ? a : b)
 #define array_size(foo) (sizeof(foo) / sizeof(foo[0]))
@@ -174,6 +175,80 @@ static void print_sockets(void *data, int each, socklen_t len, void *extra,
 	}
 }
 
+static void print_time(time_t *time)
+{
+	char buf[128];
+
+	if ((*time) == 0) {
+		printf("%-24s ", "---");
+	} else {
+		strftime(buf, sizeof(buf), "%D %H:%M:%S %Z", localtime(time));
+		printf("%-24s ", buf);
+	}
+}
+static void print_paths(void *data, int each, socklen_t len, void *extra,
+			bool prt_ipv6)
+{
+	struct rds_info_connection_paths *rds_cinfo;
+	int prt_width;
+	struct rds_path_info *pinfo;
+
+	printf("\nRDS Paths:\n");
+	if (prt_ipv6)
+		prt_width = PRT_IPV6_WIDTH;
+	else
+		prt_width = PRT_IPV4_WIDTH;
+	while (len) {
+		int i = 0;
+
+		rds_cinfo = (struct rds_info_connection_paths *)data;
+		len -= each;
+		data += each;
+		printf("\t%*s %*s %4s %7s\n", prt_width, "LocalAddr",
+		       prt_width, "RemoteAddr", "Tos", "Trans");
+		printf("\t%*s %*s %4u %7s\n",
+			prt_width, ipaddr(&rds_cinfo->local_addr, 1),
+			prt_width, ipaddr(&rds_cinfo->peer_addr, 1),
+			rds_cinfo->tos,
+			rds_cinfo->transport);
+		printf("\n%-4s %-24s %-24s %-24s %-10s %-6s %-11s %-s\n",
+		       "P", "Connected@", "Attempt@",
+		       "Reset@", "Attempts", "RDS", "Down(Secs)", "Reason");
+		pinfo = (struct rds_path_info *) (rds_cinfo + 1);
+		do {
+			printf("%-4d ", pinfo->index);
+			print_time(&pinfo->connect_time);
+			print_time(&pinfo->attempt_time);
+			print_time(&pinfo->reset_time);
+			printf("%-10d ", pinfo->connect_attempts);
+			printf("%c%c%c%c   ",
+			rds_conn_flag(pinfo->flags, SENDING,
+				      rds_cinfo->transport, 's'),
+			rds_conn_flag(pinfo->flags, CONNECTING,
+				      rds_cinfo->transport, 'c'),
+			rds_conn_flag(pinfo->flags, CONNECTED,
+				      rds_cinfo->transport, 'C'),
+			rds_conn_flag(pinfo->flags, ERROR,
+				      rds_cinfo->transport, 'E'));
+			if ((pinfo->flags & RDS_INFO_CONNECTION_FLAG_CONNECTED) &&
+			    pinfo->reset_time != 0) {
+				printf("%-11ld ",
+				       pinfo->connect_time - pinfo->reset_time);
+			} else {
+				printf("%-11s ", "---");
+			}
+			if (pinfo->disconnect_reason > MAC_DISCON_REASON)
+				printf("%15d\n", pinfo->disconnect_reason);
+			else
+				printf("%-s\n",
+				       conn_drop_reasons[pinfo->disconnect_reason]);
+			pinfo++;
+		} while (++i < rds_cinfo->npaths);
+		if (len)
+			printf("\n");
+	}
+}
+
 static void print_conns(void *data, int each, socklen_t len, void *extra,
 			bool prt_ipv6)
 {
@@ -198,10 +273,14 @@ static void print_conns(void *data, int each, socklen_t len, void *extra,
 			       conn6.tos,
 			       conn6.next_tx_seq,
 			       conn6.next_rx_seq,
-			       rds_conn_flag(conn6, SENDING, 's'),
-			       rds_conn_flag(conn6, CONNECTING, 'c'),
-			       rds_conn_flag(conn6, CONNECTED, 'C'),
-			       rds_conn_flag(conn6, ERROR, 'E'));
+			       rds_conn_flag(conn6.flags, SENDING,
+					     conn6.transport, 's'),
+			       rds_conn_flag(conn6.flags, CONNECTING,
+					     conn6.transport, 'c'),
+			       rds_conn_flag(conn6.flags, CONNECTED,
+					     conn6.transport, 'C'),
+			       rds_conn_flag(conn6.flags, ERROR,
+					     conn6.transport, 'E'));
 		}
 	} else {
 		for_each(conn, data, each, len) {
@@ -211,10 +290,14 @@ static void print_conns(void *data, int each, socklen_t len, void *extra,
 			       conn.tos,
 			       conn.next_tx_seq,
 			       conn.next_rx_seq,
-			       rds_conn_flag(conn, SENDING, 's'),
-			       rds_conn_flag(conn, CONNECTING, 'c'),
-			       rds_conn_flag(conn, CONNECTED, 'C'),
-			       rds_conn_flag(conn, ERROR, 'E'));
+			       rds_conn_flag(conn.flags, SENDING,
+					     conn.transport, 's'),
+			       rds_conn_flag(conn.flags, CONNECTING,
+					     conn.transport, 'c'),
+			       rds_conn_flag(conn.flags, CONNECTED,
+					     conn.transport, 'C'),
+			       rds_conn_flag(conn.flags, ERROR,
+					     conn.transport, 'E'));
 		}
 	}
 }
@@ -407,6 +490,8 @@ struct info infos[] = {
 		print_sockets, NULL, 0 },
 	['n'] = { RDS6_INFO_CONNECTIONS, RDS_INFO_CONNECTIONS, "connections",
 		print_conns, NULL, 0 },
+	['p'] = { RDS6_INFO_CONN_PATHS, RDS_INFO_CONN_PATHS, "paths",
+		print_paths, NULL, 0 },
 	['r'] = { RDS6_INFO_RECV_MESSAGES, RDS_INFO_RECV_MESSAGES,
 		"recv queue messages", print_msgs, "Receive", 0 },
 	['s'] = { RDS6_INFO_SEND_MESSAGES, RDS_INFO_SEND_MESSAGES,

commit 7fec557fcd871da919d73c91b775dd4e6de5486d

rds-info: Remove unnecessary headers from rds.h
Orabug: 34466041

The rds.h header file is including headers that it doesn't need. One of
these, time.h, should be rds-info.c. The other is just not needed.

Fixes: eb793ecd0fef ("rds-tools: Added '-p' option to rds-info to show paths")
Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>

include/rds.h
@@ -41,8 +41,6 @@
 
 #include <linux/types.h>
 #include <limits.h>
-#include <linux/net.h>
-#include <time.h>
 /* XXX <net/sock.h> was included as part of NETFILTER support (commit f13bbf62)
  * but <net/sock.h> is not exported to uapi, although <linux/rds.h> is
  * (in theory). Is <net/sock.h> needed for user-apps that use netfilter?

rds-info.c
@@ -47,6 +47,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "rds.h"
 #include "pfhack.h"
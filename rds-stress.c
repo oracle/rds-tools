oracle/rds-tools

commit 3baa4956ff5304dea862396efab599c0f2aafc68

rds-stress: Add long option syntax to usage output
The usage output should include the long option syntax.

Orabug: 32218037

Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: William Kucharski <william.kucharski@oracle.com>

rds-stress.c
@@ -461,26 +461,26 @@ static void usage(void)
 	fprintf(stderr,
 	"\n"
 	"Send & Recv parameters:\n"
-	" -r [addr]         use this local address\n"
-	" -p [port, 4000]   starting port number\n"
+	" -r, --recv-addr [addr]    use this local address\n"
+	" -p, --port [port]         starting port number (default is 4000)\n"
 	"\n"
 	"Send parameters:\n"
-	" -s [addr]           send to this address (required)\n"
-	" -a [bytes, [%u|%u]] ack message length\n"
-	" -q [bytes, 1024]    request message length\n"
-	" -o                  datagrams sent one way only (default is both)\n"
-	" -d [depth, 1]       request pipeline depth, nr outstanding\n"
-	" -t [nr, 1]          number of child tasks\n"
-	" -T [seconds, 0]     runtime of test, 0 means infinite\n"
-	" -Q [tos, 0]         Type of Service\n"
-	" -D [bytes]          RDMA: size\n"
-	" -I [iovecs, 1]      RDMA: number of user buffers to target (max 512)\n"
-	" -M [nr, 0]          RDMA: mode (0=readwrite,1=readonly,2=writeonly)\n"
+	" -s, --send-addr [addr]    send to this address (required)\n"
+	" -a, --ack-bytes [bytes]   ack message length\n"
+	" -q, --req-bytes [bytes]   request message length (default is 1024)\n"
+	" -o,                       datagrams sent one way only (default is both)\n"
+	" -d, --depth [depth]       request pipeline depth, nr outstanding (default is 1)\n"
+	" -t, --tasks [nr]          number of child tasks (default is 1)\n"
+	" -T, --time [seconds]      runtime of test, 0 means infinite (default is 0)\n"
+	" -Q, --tos [tos]           Type of Service (default is 0)\n"
+	" -D, --rdma-bytes [bytes]  RDMA: size\n"
+	" -I [iovecs]               RDMA: number of user buffers to target (max 512) (default is 1)\n"
+	" -M [nr]                   RDMA: mode (0=readwrite,1=readonly,2=writeonly) (default is 0)\n"
 	"\n"
 	"Optional flags:\n"
-	" -c                  measure cpu use with per-cpu soak processes\n"
-	" -V                  trace execution\n"
-	" -z                  print a summary at end of test only\n"
+	" -c, --report-cpu          measure cpu use with per-cpu soak processes\n"
+	" -V, --trace               trace execution\n"
+	" -z, --report-summary      print a summary at end of test only\n"
 	"\n"
 	"Example:\n"
 	"  recv$ rds-stress\n"

commit b55a0d838ee0d9a20217944d9ff774cd046ee69a

rds-stress: Fix usage() message compiler warning
Orabug: 34484319

The fix introduced by 3baa4956 ("rds-stress: Add long option syntax to
usage output") introduced a build warning:

rds-stress.c: In function 'usage':
rds-stress.c:465:2: warning: too many arguments for format

The usage print statement is missing format specifiers for one of its
options. Reviewing the original print specification, the arguments are
used to provide the default values for IPv4 and IPv6 ack message
lengths.

Fixes: 3baa4956ff53 ("rds-stress: Add long option syntax to usage output")
Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

rds-stress.c
@@ -469,7 +469,7 @@ static void usage(void)
 	"\n"
 	"General Options:\n"
 	" -s, --send-addr [addr]        send to this address (required by Send)\n"
-	" -a, --ack-bytes [bytes]       ack message length\n"
+	" -a, --ack-bytes [bytes]       ack message length (default is %u for IPv4 and %u for IPv6)\n"
 	" -q, --req-bytes [bytes]       request message length (default is 1024)\n"
 	" -o, --one-way                 datagrams sent one way only (default is both)\n"
 	" -d, --depth [depth]           request pipeline depth, nr outstanding (default is 1)\n"
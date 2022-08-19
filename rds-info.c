oracle/rds-tools

commit 7be4f24cffc13f97ce1c9833096ee24516abbed6

rds-info: extend rds-info -Iv functionality
A recent change in kernel exports additional diagnostic information for
rds-info -Iv. This patch is the corresponding user-space change which
allows these additional fields to be displayed.

rds-info has also been upgraded to support a new '-o' option to
selectively display the information. The man page documents the new
fields available along with examples. This change does not break
compatibility with older kernels which do not export these additional
fields.

Orabug: 33754433

Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
Reviewed-by: Manjunath Patil <manjunath.b.patil@oracle.com>

rds-info.c
@@ -69,6 +69,8 @@
 	__ret;						\
 })
 
+#define kB(a) ((float)a / 1024.0)
+
 #define for_each(var, data, each, len) 			\
 	for (;len > 0 && copy_into(var, data, each);	\
 	     data += each, len -= min(len, each))
@@ -78,7 +80,11 @@
                 fprintf((f), fmt, ##a); \
 } while (0)
 
+#define ADD_FIELD_STR_LEN 256
+
 static int	opt_verbose = 0;
+static int	opt_add;
+char		add_fields[ADD_FIELD_STR_LEN] = "";
 
 char *progname = "rds-info";
 
@@ -432,10 +438,55 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 	else
 		prt_width = PRT_IPV4_WIDTH;
 
+	if (opt_verbose && (!opt_add)) {
+		memset(add_fields, 0, ADD_FIELD_STR_LEN);
+		strcat(add_fields, "cache_allocs");
+		strcat(add_fields, ", recv_alloc_ctr");
+		strcat(add_fields, ", recv_free_ctr");
+		strcat(add_fields, ", send_alloc_ctr");
+		strcat(add_fields, ", send_free_ctr");
+		strcat(add_fields, ", send_bytes");
+		strcat(add_fields, ", recv_bytes");
+		strcat(add_fields, ", r_read_bytes");
+		strcat(add_fields, ", r_write_bytes");
+		strcat(add_fields, ", tx_poll_ts");
+		strcat(add_fields, ", rx_poll_ts");
+		strcat(add_fields, ", tx_poll_cnt");
+		strcat(add_fields, ", rx_poll_cnt");
+	}
 
 	printf("\nRDS IB Connections:\n%*s %*s %4s %3s %32s %32s %10s %10s",
 	       prt_width, "LocalAddr", prt_width, "RemoteAddr", "Tos", "SL",
 	       "LocalDev", "RemoteDev", "SrcQPNo", "DstQPNo");
+
+	if (opt_add || opt_verbose) {
+		if (strcasestr(add_fields, "cache_allocs"))
+			printf("%15s", "Cache Allocs");
+		if (strcasestr(add_fields, "recv_alloc_ctr"))
+			printf("%15s", "Recv_alloc_ctr");
+		if (strcasestr(add_fields, "recv_free_ctr"))
+			printf("%15s", "Recv_free_ctr");
+		if (strcasestr(add_fields, "send_alloc_ctr"))
+			printf("%15s", "Send_alloc_ctr");
+		if (strcasestr(add_fields, "send_free_ctr"))
+			printf("%15s", "Send_free_ctr");
+		if (strcasestr(add_fields, "send_bytes"))
+			printf("%16s", "Send_bytes KiB");
+		if (strcasestr(add_fields, "recv_bytes"))
+			printf("%16s", "Recv_bytes KiB");
+		if (strcasestr(add_fields, "r_read_bytes"))
+			printf("%19s", "R_read_bytes KiB");
+		if (strcasestr(add_fields, "r_write_bytes"))
+			printf("%19s", "R_write_bytes KiB");
+		if (strcasestr(add_fields, "tx_poll_ts"))
+			printf("%15s", "Tx_poll_ts_ms");
+		if (strcasestr(add_fields, "rx_poll_ts"))
+			printf("%15s", "Rx_poll_ts_ms");
+		if (strcasestr(add_fields, "tx_poll_cnt"))
+			printf("%15s", "Tx_poll_cnt");
+		if (strcasestr(add_fields, "rx_poll_cnt"))
+			printf("%15s", "Rx_poll_cnt");
+	}
 
 	printf("\n");
 
@@ -450,13 +501,33 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 			       ic6.qp_num,
 			       ic6.dst_qp_num);
 
-			if (opt_verbose) {
-				printf("  send_wr=%u", ic6.max_send_wr);
-				printf(", recv_wr=%u", ic6.max_recv_wr);
-				printf(", send_sge=%u", ic6.max_send_sge);
-				printf(", rdma_mr_max=%u", ic6.rdma_mr_max);
-				printf(", rdma_mr_size=%u", ic6.rdma_mr_size);
-				printf(", cache_allocs=%u", ic6.cache_allocs);
+			if (opt_add || opt_verbose) {
+				if (strcasestr(add_fields, "cache_allocs"))
+					printf("%15"PRIu32, ic6.cache_allocs);
+				if (strcasestr(add_fields, "recv_alloc_ctr"))
+					printf("%15"PRIu32, ic6.recv_alloc_ctr);
+				if (strcasestr(add_fields, "recv_free_ctr"))
+					printf("%15"PRIu32, ic6.recv_free_ctr);
+				if (strcasestr(add_fields, "send_alloc_ctr"))
+					printf("%15"PRIu32, ic6.send_alloc_ctr);
+				if (strcasestr(add_fields, "send_free_ctr"))
+					printf("%15"PRIu32, ic6.send_free_ctr);
+				if (strcasestr(add_fields, "send_bytes"))
+					printf("%16.2f", kB(ic6.send_bytes));
+				if (strcasestr(add_fields, "recv_bytes"))
+					printf("%16.2f", kB(ic6.recv_bytes));
+				if (strcasestr(add_fields, "r_read_bytes"))
+					printf("%19.2f", kB(ic6.r_read_bytes));
+				if (strcasestr(add_fields, "r_write_bytes"))
+					printf("%19.2f", kB(ic6.r_write_bytes));
+				if (strcasestr(add_fields, "tx_poll_ts"))
+					printf("%15"PRIu64, ic6.tx_poll_ts);
+				if (strcasestr(add_fields, "rx_poll_ts"))
+					printf("%15"PRIu64, ic6.rx_poll_ts);
+				if (strcasestr(add_fields, "tx_poll_cnt"))
+					printf("%15"PRIu64, ic6.tx_poll_cnt);
+				if (strcasestr(add_fields, "rx_poll_cnt"))
+					printf("%15"PRIu64, ic6.rx_poll_cnt);
 			}
 
 			printf("\n");
@@ -472,13 +543,33 @@ static void print_ib_conns(void *data, int each, socklen_t len, void *extra,
 			       ic.qp_num,
 			       ic.dst_qp_num);
 
-			if (opt_verbose) {
-				printf("  send_wr=%u", ic.max_send_wr);
-				printf(", recv_wr=%u", ic.max_recv_wr);
-				printf(", send_sge=%u", ic.max_send_sge);
-				printf(", rdma_mr_max=%u", ic.rdma_mr_max);
-				printf(", rdma_mr_size=%u", ic.rdma_mr_size);
-				printf(", cache_allocs=%u", ic.cache_allocs);
+			if (opt_add || opt_verbose) {
+				if (strcasestr(add_fields, "cache_allocs"))
+					printf("%15"PRIu32, ic.cache_allocs);
+				if (strcasestr(add_fields, "recv_alloc_ctr"))
+					printf("%15"PRIu32, ic.recv_alloc_ctr);
+				if (strcasestr(add_fields, "recv_free_ctr"))
+					printf("%15"PRIu32, ic.recv_free_ctr);
+				if (strcasestr(add_fields, "send_alloc_ctr"))
+					printf("%15"PRIu32, ic.send_alloc_ctr);
+				if (strcasestr(add_fields, "send_free_ctr"))
+					printf("%15"PRIu32, ic.send_free_ctr);
+				if (strcasestr(add_fields, "send_bytes"))
+					printf("%16.2f", kB(ic.send_bytes));
+				if (strcasestr(add_fields, "recv_bytes"))
+					printf("%16.2f", kB(ic.recv_bytes));
+				if (strcasestr(add_fields, "r_read_bytes"))
+					printf("%19.2f", kB(ic.r_read_bytes));
+				if (strcasestr(add_fields, "r_write_bytes"))
+					printf("%19.2f", kB(ic.r_write_bytes));
+				if (strcasestr(add_fields, "tx_poll_ts"))
+					printf("%15"PRIu64, ic.tx_poll_ts);
+				if (strcasestr(add_fields, "rx_poll_ts"))
+					printf("%15"PRIu64, ic.rx_poll_ts);
+				if (strcasestr(add_fields, "tx_poll_cnt"))
+					printf("%15"PRIu64, ic.tx_poll_cnt);
+				if (strcasestr(add_fields, "rx_poll_cnt"))
+					printf("%15"PRIu64, ic.rx_poll_cnt);
 			}
 
 			printf("\n");
@@ -546,7 +637,7 @@ static void print_usage(int rc)
 
 int main(int argc, char **argv)
 {
-	char optstring[258] = "av+";
+	char optstring[258] = "ao:v+";
 	int given_options = 0;
 	socklen_t len = 0;
 	void *data = NULL;
@@ -574,6 +665,10 @@ int main(int argc, char **argv)
 
 	while ((c = getopt(argc, argv, optstring)) != EOF) {
 		switch (c) {
+		case 'o':
+			strncpy(add_fields, optarg, (ADD_FIELD_STR_LEN - 1));
+			opt_add++;
+			continue;
 		case 'v':
 			opt_verbose++;
 			continue;

include/rds.h
@@ -303,10 +303,19 @@ struct rds_info_rdma_connection {
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
 	int32_t		qp_num;
-	uint32_t        w_alloc_ctr;
-	uint32_t        w_free_ctr;
+	uint32_t        recv_alloc_ctr;
+	uint32_t        recv_free_ctr;
 	int32_t		dst_qp_num;
-
+	uint32_t	send_alloc_ctr;
+	uint32_t	send_free_ctr;
+	uint64_t	send_bytes;
+	uint64_t	recv_bytes;
+	uint64_t	r_read_bytes;	/* rdma_read_bytes */
+	uint64_t	r_write_bytes;	/* rdma_write_bytes */
+	uint64_t	tx_poll_ts;
+	uint64_t	rx_poll_ts;
+	uint64_t	tx_poll_cnt;
+	uint64_t	rx_poll_cnt;
 };
 
 struct rds6_info_rdma_connection {
@@ -327,9 +336,19 @@ struct rds6_info_rdma_connection {
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
 	int32_t		qp_num;
-	uint32_t	w_alloc_ctr;
-	uint32_t	w_free_ctr;
+	uint32_t	recv_alloc_ctr;
+	uint32_t	recv_free_ctr;
 	int32_t		dst_qp_num;
+	uint32_t	send_alloc_ctr;
+	uint32_t	send_free_ctr;
+	uint64_t	send_bytes;
+	uint64_t	recv_bytes;
+	uint64_t	r_read_bytes;	/* rdma_read_bytes */
+	uint64_t	r_write_bytes;	/* rdma_write_bytes */
+	uint64_t	tx_poll_ts;
+	uint64_t	rx_poll_ts;
+	uint64_t	tx_poll_cnt;
+	uint64_t	rx_poll_cnt;
 };
 
 struct rds_path_info {

rds-info.1
@@ -140,9 +140,44 @@ The source QP number for this connection.
 The destination QP number for this connection.
 .El
 
-If verbose output is requested, per-connection settings such as the
-maximum number of send and receive work requests will be displayed
-in addition.
+If verbose output is requested, additional per-connection information
+will be displayed. These additional fields are documented below.
+
+.Bl -tag -width 4
+.It CacheAllocs
+The total number of cache allocs used by this connection.
+.It RecvAllocCtr
+The RDS IB connection recv ring alloc counter.
+.It RecvFreeCtr
+The RDS IB connection recv ring free counter.
+.It SendAllocCtr
+The RDS IB connection send ring alloc counter.
+.It SendFreeCtr
+The RDS IB connection send ring free counter.
+.It SendBytes
+The total number of bytes sent by this connection.
+.It RecvBytes
+The total number of bytes received by this connection.
+.It RReadBytes
+The total number of RDMA bytes read by this connection.
+.It RWriteBytes
+The total number of RDMA bytes written by this connection.
+.It TxPollTs
+The time in msec from last scq[send completion queue] poll.
+.It RxPollTs
+The time in msec from last rcq[recv completion queue] poll.
+.It TxPollCnt
+The number of scq polls so far, by RDS on this connection.
+.It RxPollCnt
+The number of rcq polls so far, by RDS on this connection.
+.El
+
+The "-I" option also supports the use of the "-o" flag in addition to it
+to allow the user to selectively display the additional fields documented
+above.
+
+Example:	rds-info -I -o "rx_poll_cnt, r_read_bytes, recv_alloc_ctr"
+		rds-info -I -o "cache_allocs, send_bytes, tx_poll_ts"
 
 .It Fl T
 Display the TCP sockets which the TCP transport is using to provide

commit 991338bd002e02b84cf58de081785b6990bec861

rds-info: Fix strcasestr() implicit declaration warning
Orabug: 34484319

The fix in 7be4f24c ("rds-info: extend rds-info -Iv functionality")
introduced the use of the strcasestr() function. It is defined in
string.h, but must be preceded by "#define _GNU_SOURCE" as it is a
non-standard extension. Without that, a warning about an implicit
declaration of the function is printed to stderr.

warning: implicit declaration of function 'strcasestr'

Fixes: 7be4f24cffc1 ("rds-info: extend rds-info -Iv functionality")
Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

rds-info.c
@@ -35,6 +35,7 @@
  * vim: noexpandtab sw=8 ts=8 sts=0:
  */
 
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>